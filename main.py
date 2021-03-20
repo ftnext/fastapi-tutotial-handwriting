63b846212bf0597cd5be05d4967d8879999103b5
ftnext <takuyafjp+develop@gmail.com>
2021-03-20 21:02:20 +0900

    [feat] Path parameter: order matters

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 async def read_item(item_id: int):  # path operation function
     # 型ヒントと合わないパラメタの場合は422  Unprocessable Entity
     return {"item_id": item_id}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/users/me")  # 上に定義されていない場合、全てread_userで実行されてしまう
+async def read_user_me():
+    return {"user_id": "the current user"}
