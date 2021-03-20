7ceb3ad2e0c9b536c533834de3de3130eba113a3
ftnext <takuyafjp+develop@gmail.com>
2021-03-20 20:57:57 +0900

    [feat] Path parameter with type hint

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")  # path operation decorator (path: / . operation: get)
-async def root():  # path operation function
-    return {"massage": "Hello World"}
+@app.get("/items/{item_id}")  # path operation decorator (path: / . operation: get)
+async def read_item(item_id: int):  # path operation function
+    # 型ヒントと合わないパラメタの場合は422  Unprocessable Entity
+    return {"item_id": item_id}
